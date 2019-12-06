2e385df62841c64fb0c4679bc3410642e1c81364
JeffRisberg <jrisberg@aol.com>
2019-12-06 14:59:59 -0800

    updated example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,40 @@ import org.junit.Test;
 
 public class Main {
 
-  @Test
-  public void login_to_chat_bot() {
+  String theToken;
 
-    when().
-      get("/lotto/{id}", 5).
+  @Before
+  public void login_to_chat_bot() {
+    given().request().body("username, password").
+      when().
+      post("/chatbot.dev1.aisera.net/aisera/webchat/login").
       then().
       statusCode(200).
-      body("lotto.lottoId", equalTo(5),
-        "lotto.winners.winnerId", hasItems(23, 54));
+    // response provides a token, extract it, and sstore into theToken
 
+  }
+
+  @Test
+  public void make_a_request() {
+    given().request().header("authorization", "Mutual " + theToken)
+      .header("get-over-post", true)
+      .body("query to webchat")
+    when().
+      post("/chatbot.dev1.aisera.net/aisera/webchat/receive").
+      then().
+      statusCode(200).
+      body(" // this should match what is in the document from Ajay");
+  }
+
+  @Test
+  public void make_another_request() {
+    given().request().header("authorization", "Mutual " + theToken)
+      .header("get-over-post", true)
+      .body("second query to webchat")
+    when().
+      post("/chatbot.dev1.aisera.net/aisera/webchat/receive").
+      then().
+      statusCode(200).
+      body(" // this second response");
   }
 }
